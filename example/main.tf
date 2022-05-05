f84040041bbe94c1be0d528d22d9a414ad9ab872
chrisdeffer <christopher.munson@sourcefuse.com>
2022-05-05 16:07:43 -0400

    feature/REF-118-ecs-module: module works...cleaning up stuff

--- example/main.tf
+++ example/main.tf
@@ -1,4 +1,17 @@
 module "ecs_fargate" {
   source = "../."
-  subnets = var.subnets
+  profile                 = "poc2"
+  vpc_id                  = "vpc-04336471107d1b7c2"
+  environment             = "dev"
+  name                    = "ecs-fargate"
+  health_check_path       = "/"
+  alb_security_groups     = [] // Grab from console
+  subnets                 = ["subnet-02a09c9b7b22ba00a","subnet-0f76d443d6aa1891b"] // Grab from console
+  alb_tls_cert_arn        = [] // Grab from console
+  service_desired_count   = "3"
+  container_port          = 80
+  region                  = "us-east-1"
+  container_image         = "nginx"
+  container_cpu           = 256
+  container_memory        = 512
 }
