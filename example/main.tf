b8f5869564e97362d2124c401e3a6be1aa4de2ad
chrisdeffer <christopher.munson@sourcefuse.com>
2022-05-09 11:10:13 -0400

    feature/REF-118-ecs-module

--- example/main.tf
+++ example/main.tf
@@ -13,3 +13,18 @@ module "ecs_fargate" {
   container_cpu         = var.container_cpu
   container_memory      = var.container_memory
 }
+
+module "ecs_service_fargate" {
+  source                = "../health_check_service"
+  environment           = var.environment
+  name                  = var.name
+  health_check_path     = var.health_check_path
+  subnets               = var.subnets
+  alb_tls_cert_arn      = var.alb_tls_cert_arn
+  service_desired_count = var.service_desired_count
+  container_port        = var.container_port
+  region                = var.region
+  container_image       = var.container_image
+  container_cpu         = var.container_cpu
+  container_memory      = var.container_memory
+}
