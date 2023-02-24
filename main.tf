4c82c705a52c70782815f07600ac95fd3f46cecc
Travis Saucier <travis.saucier@sourcefuse.com>
2023-02-24 13:26:24 -0500

    add aws blueprint configuration. remove health check to make it an independent module.

--- main.tf
+++ main.tf
@@ -13,11 +13,22 @@ terraform {
 }
 
 ################################################################################
-## ecs cluster
+## ecs
 ################################################################################
+## cluster
 module "ecs" {
   source       = "git::https://github.com/terraform-aws-modules/terraform-aws-ecs?ref=v4.1.2"
   cluster_name = local.cluster_name
+
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+
+      log_configuration = {
+        cloud_watch_log_group_name = aws_cloudwatch_log_group.this.name
+      }
+    }
+  }
 
   tags = merge(var.tags, tomap({
     Name = local.cluster_name
@@ -25,101 +36,112 @@ module "ecs" {
 }
 
 ################################################################################
-## iam
+## logging
 ################################################################################
-data "aws_iam_policy_document" "assume_role_policy" {
+resource "aws_cloudwatch_log_group" "this" {
+  name = "/aws/ecs/${module.ecs.cluster_name}"
+
+  retention_in_days = var.log_group_retention_days
+  skip_destroy      = var.log_group_skip_destroy
+
+  tags = merge(var.tags, tomap({
+    Name = "/aws/ecs/${module.ecs.cluster_name}"
+  }))
+}
+
+################################################################################
+## service discovery namespaces
+################################################################################
+resource "aws_service_discovery_private_dns_namespace" "this" {
+  for_each = toset(var.service_discovery_private_dns_namespace)
+
+  name        = "${each.key}.${module.ecs.cluster_name}.local"
+  description = "Service discovery for ${each.key}.${module.ecs.cluster_name}.local"
+  vpc         = var.vpc_id
+}
+
+################################################################################
+## task execution role
+################################################################################
+resource "aws_iam_role" "execution" {
+  name_prefix        = "${module.ecs.cluster_name}-execution-"
+  assume_role_policy = data.aws_iam_policy_document.execution.json
+
+  tags = merge(var.tags, tomap({
+    NamePrefix = "${module.ecs.cluster_name}-execution-"
+  }))
+}
+
+data "aws_iam_policy_document" "execution" {
   statement {
-    effect  = "Allow"
     actions = ["sts:AssumeRole"]
 
     principals {
-      type = "Service"
-      identifiers = [
-        "ecs-tasks.amazonaws.com"
-      ]
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
 
-// TODO: clean up and split task execution role from task role
-resource "aws_iam_role" "health_check_ecs_role" {
-  name = "${module.ecs.cluster_name}-health-check"
+resource "aws_iam_policy_attachment" "execution" {
+  for_each = toset(var.execution_policy_attachment_arns)
 
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  name       = "${module.ecs.cluster_name}-execution"
+  roles      = [aws_iam_role.execution.name]
+  policy_arn = each.value
+}
 
-  inline_policy {
-    name = "admin_policy_that_needs_to_go"
-
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = "*"
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-    })
-  }
+resource "aws_iam_policy" "secrets_manager_read_policy" {
+  name = "ECSTaskExecutionReadSecretsManager"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Resource = "*"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ],
+      }
+    ]
+  })
 
   tags = merge(var.tags, tomap({
-    Name = "${module.ecs.cluster_name}-health-check"
+    Name = "ECSTaskExecutionReadSecretsManager"
   }))
 }
 
-resource "aws_iam_role_policy_attachment" "aws_service_linked_role" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
-  role       = aws_iam_role.health_check_ecs_role.name
+resource "aws_iam_policy_attachment" "secret_manager_read" {
+  name       = "${module.ecs.cluster_name}-execution-policy"
+  roles      = [aws_iam_role.execution.name]
+  policy_arn = aws_iam_policy.secrets_manager_read_policy.arn
 }
 
 ################################################################################
-## ecs task definition
+## load balancer
 ################################################################################
-resource "aws_ecs_task_definition" "health_check_task_definition" {
-  family                   = "${module.ecs.cluster_name}-health-check"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = 1024 // TODO - change to variable
-  memory                   = 2048 // TODO - change to variable
-  task_role_arn            = aws_iam_role.health_check_ecs_role.arn
-  execution_role_arn       = aws_iam_role.health_check_ecs_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "${module.ecs.cluster_name}-health-check-nginx"
-      image     = "nginx"
-      cpu       = 100
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    }
-  ])
-
-  tags = merge(var.tags, tomap({
-    Name = "${module.ecs.cluster_name}-health-check"
-  }))
-}
-
-################################################################################
-## security
-################################################################################
-resource "aws_security_group" "ecs_task_sg" {
+resource "aws_security_group" "alb" {
+  name   = "${module.ecs.cluster_name}-alb"
   vpc_id = var.vpc_id
-  name   = "${module.ecs.cluster_name}-ecs-task-sg"
 
+  // TODO - make dynamic
   ingress {
-    from_port        = 0
+    from_port        = 80
     protocol         = "-1"
-    to_port          = 0
+    to_port          = 80
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    from_port        = 443
+    protocol         = "-1"
+    to_port          = 443
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  // TODO - tighten this down to only go to the task subnets
   egress {
     from_port        = 0
     protocol         = "-1"
@@ -129,40 +151,11 @@ resource "aws_security_group" "ecs_task_sg" {
   }
 
   tags = merge(var.tags, tomap({
-    Name = "${module.ecs.cluster_name}-ecs-task-sg"
+    Name = "${module.ecs.cluster_name}-alb"
   }))
 }
 
-################################################################################
-## ecs service
-################################################################################
-resource "aws_ecs_service" "health_check_service" {
-  name            = "${module.ecs.cluster_name}-health-check"
-  cluster         = module.ecs.cluster_id
-  task_definition = aws_ecs_task_definition.health_check_task_definition.arn
-  launch_type     = "FARGATE"
-  desired_count   = 3
-
-  network_configuration {
-    subnets          = var.task_subnet_ids
-    security_groups  = [aws_security_group.ecs_task_sg.id]
-    assign_public_ip = false
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.health_check_target_group.arn
-    container_name   = "${module.ecs.cluster_name}-health-check-nginx"
-    container_port   = 80
-  }
-
-  tags = merge(var.tags, tomap({
-    Name = "${module.ecs.cluster_name}-health-check"
-  }))
-}
-
-################################################################################
-## load balancer
-################################################################################
+## alb
 module "alb" {
   source = "./modules/alb"
 
@@ -170,7 +163,7 @@ module "alb" {
   environment        = var.environment
   vpc_id             = var.vpc_id
   subnet_ids         = var.alb_subnets_ids
-  security_group_ids = [aws_security_group.ecs_task_sg.id]
+  security_group_ids = [aws_security_group.alb.id]
 
   access_logs_enabled                             = true  // TODO - change to variable
   alb_access_logs_s3_bucket_force_destroy         = false // TODO - change to variable
@@ -191,63 +184,24 @@ module "alb" {
   tags = var.tags
 }
 
-## target group
-resource "aws_lb_target_group" "health_check_target_group" {
-  name        = "${module.ecs.cluster_name}-hc"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = var.vpc_id
+## health check
+module "health_check" {
+  source = "./modules/health-check"
 
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    timeout             = "3"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
+  vpc_id     = var.vpc_id
+  subnet_ids = length(var.health_check_subnet_ids) > 0 ? var.health_check_subnet_ids : var.alb_subnets_ids
 
-  tags = merge(var.tags, tomap({
-    Name = "${module.ecs.cluster_name}-hc"
-  }))
-}
+  cluster_id   = module.ecs.cluster_id
+  cluster_name = module.ecs.cluster_name
 
-## https forward
-resource "aws_lb_listener" "https_default" {
-  load_balancer_arn = module.alb.alb_arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = var.alb_ssl_policy
-  certificate_arn   = var.alb_acm_certificate_arn
+  lb_arn                 = module.alb.alb_arn
+  lb_acm_certificate_arn = var.alb_acm_certificate_arn
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.health_check_target_group.arn
-  }
+  lb_security_group_ids = [
+    aws_security_group.alb.id
+  ]
 
-  tags = merge(var.tags, tomap({
-    Name = "${module.ecs.cluster_name}-https-forward"
-  }))
-}
-
-## http redirect
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = module.alb.alb_arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-
-  tags = merge(var.tags, tomap({
-    Name = "${module.ecs.cluster_name}-http-redirect"
-  }))
+  depends_on = [
+    module.alb
+  ]
 }
