268defd3cd45eb43a71479b8fbea1064f19b4119
Arunsai14 <arun.sai@sourcefuse.com>
2025-03-03 20:01:38 +0530

    adding ec2

--- modules/ecs-service/data.tf
+++ modules/ecs-service/data.tf
@@ -20,6 +20,6 @@ data "aws_ecs_cluster" "cluster" {
   cluster_name = var.ecs_service.cluster_name
 }
 
-data "aws_lb_target_group" "ecs_target_group" {
-  name = var.ecs_service.aws_lb_target_group_name
-}
+# data "aws_lb_target_group" "ecs_target_group" {
+#   name = var.ecs_service.aws_lb_target_group_name
+# }
