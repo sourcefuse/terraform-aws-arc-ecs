f41ec841531d560cd4862ad394c41d46e0de3be3
Arunsai14 <arun.sai@sourcefuse.com>
2025-03-03 19:39:09 +0530

    adding ec2

--- outputs.tf
+++ outputs.tf
@@ -9,12 +9,12 @@
 # }
 
 
-output "ecs_service_name" {
-  description = "The service names of the ECS services."
-  value       = module.ecs_service[*].service_name
-}
+# output "ecs_service_name" {
+#   description = "The service names of the ECS services."
+#   value       = module.ecs_service[*].service_name
+# }
 
-output "ecs_task_definition_arn" {
-  description = "The ARNs of the ECS task definitions."
-  value       = module.ecs_service[*].task_definition_arn
-}
+# output "ecs_task_definition_arn" {
+#   description = "The ARNs of the ECS task definitions."
+#   value       = module.ecs_service[*].task_definition_arn
+# }
