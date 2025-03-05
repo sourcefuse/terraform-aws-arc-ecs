f0149f275235d29badb71d07eb85e14f826587f5
Arunsai14 <arun.sai@sourcefuse.com>
2025-03-05 06:13:02 +0530

    adding ec2

--- examples/ecs-ec2/outputs.tf
+++ examples/ecs-ec2/outputs.tf
@@ -5,15 +5,15 @@ output "ecs_cluster_name" {
 
 output "ecs_service_name" {
   description = "The name of the ECS service"
-  value       = module.ecs_services[*].service_name
+  value       = module.ecs_services.service_name
 }
 
 output "ecs_service_arn" {
   description = "The ARN of the ECS service"
-  value       = module.ecs_services[*].ecs_service_arn
+  value       = module.ecs_services.ecs_service_arn
 }
 
 output "ecs_task_definition_arn" {
   description = "The ARN of the ECS task definition"
-  value       = module.ecs_services[*].ecs_task_definition_arn
+  value       = module.ecs_services.ecs_task_definition_arn
 }
